8dadb5db47558873fc8edbe05ce6b8f5dd664fb3
clockwerkz <clockwerkz@gmail.com>
2019-01-05 23:26:34 -0800

    update functions in MongoDB course lesson covered - CRUD operations done

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -6,14 +6,47 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=> {
         return console.log('Unable to connect to MongoDB');
     } 
     console.log('Connected to MongoDB server');
-    db.collection('Todos')
-        .findOneAndUpdate({text : "Eat Lunch"},
-        {$set:{
-            completed: true
+    // db.collection('Todos')
+    //     .findOneAndUpdate({_id: ObjectID("5c31a80e13550caa147609b0")},
+    //     {$set:{
+    //         completed: true
+    //         }
+    //     },{
+    //         returnOriginal : false
+    //     })
+    //     .then(result => console.log(JSON.stringify(result, undefined, 2)));
+
+    // db.collection('Users')
+    //     .findOneAndUpdate(
+    //         {
+    //             _id: ObjectID("5c30460e72e9ba0cd8715573")
+    //         },
+    //         {
+    //             $inc: {
+    //                 age: -31
+    //             }
+    //         },
+    //         {
+    //             returnOriginal : false
+    //         }
+    //     )
+    //     .then(results => console.log(JSON.stringify(results, undefined, 2)));
+
+    db.collection('Users')
+        .findOneAndUpdate(
+            {
+                _id: ObjectID("5c30464aef36a10e94a471f6")
+            },{
+                $set: {
+                    name: 'Noah'
+                },
+                $inc : {
+                    age : -33
+                }
+            },{
+                returnOriginal: false
             }
-        },{
-            returnOriginal : false
-        })
-        .then(result => console.log(JSON.stringify(result, undefined, 2)));
+        )
+        .then(results => console.log(JSON.stringify(results, undefined, 2)));
     //db.close();
 });
