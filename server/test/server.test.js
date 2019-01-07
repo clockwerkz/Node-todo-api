2d2d9714fe91303b215d8a18e881e8dfa3a9583a
clockwerkz <clockwerkz@gmail.com>
2019-01-06 23:56:08 -0800

    uncomments first unit test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -11,27 +11,27 @@ beforeEach((done)=> {
 });
 
 describe('POST /todos', () => {
-    // it('should create a new todo', (done)=> {
-    //     const text = 'Test todo text';
+    it('should create a new todo', (done)=> {
+        const text = 'Test todo text';
 
-    // request(app)
-    //     .post('/todos')
-    //     .send({text})
-    //     .expect(200)
-    //     .expect((res) => {
-    //         expect(res.body.text).toBe(text);
-    //     })
-    //     .end((err, res)=>{
-    //         if (err) {
-    //            return done(err); 
-    //         }
-    //         Todo.find().then((todos) => {
-    //             expect(todos.length).toBe(1);
-    //             expect(todos[0].text).toBe(text);
-    //             done();
-    //         }).catch(err => console.log("Error:", err));
-    //     });
-    // });
+    request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.text).toBe(text);
+        })
+        .end((err, res)=>{
+            if (err) {
+               return done(err); 
+            }
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch(err => console.log("Error:", err));
+        });
+    });
 
     it('should not create Todo with invalid body date', (done)=> {
         request(app)
