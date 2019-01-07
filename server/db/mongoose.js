44e29ca87f0f1151a7dabe450a818966580f3a25
Carlos Fins <clockwerkz@gmail.com>
2019-01-06 18:20:07 -0800

    adds a note for Promise setting

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; //sets Promises to be JS Promise handling
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports.mongoose = {mongoose}
