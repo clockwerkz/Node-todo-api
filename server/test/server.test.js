345386bed6ca02f6dc2548b664cc780734245917
clockwerkz <clockwerkz@gmail.com>
2019-01-09 00:56:52 -0800

    adds get todos unit testing

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,10 +4,18 @@ const expect = require('expect');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{
+    text:'First test todo'
+},{
+    text:'Second test todo'
+}]
+
 
 beforeEach((done)=> {
     Todo.remove({})
-        .then(() => done());
+        .then(() => {
+            return Todo.insertMany(todos);
+        }).then(()=> done());
 });
 
 describe('POST /todos', () => {
@@ -25,7 +33,7 @@ describe('POST /todos', () => {
             if (err) {
                return done(err); 
             }
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -43,10 +51,20 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then(todos => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 })
                 .catch(err => done(err));
         });
-    });      
+    }); 
+
+    it('should get all Todos', (done)=> {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=> {
+            expect(res.body.data.length).toBe(2);
+        })
+        .end(done);
+    });     
 });
