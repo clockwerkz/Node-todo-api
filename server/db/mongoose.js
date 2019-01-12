186cba03f3259e78f33e54b49e1ef679916abc84
clockwerkz <clockwerkz@gmail.com>
2019-01-12 01:04:51 -0800

    configures app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //sets Promises to be JS Promise handling
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(process.env.MONGGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports.mongoose = {mongoose}
