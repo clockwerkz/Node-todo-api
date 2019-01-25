845883f7478d13edbf1ef3ddb50ef0942388e6df
clockwerkz <clockwerkz@gmail.com>
2019-01-24 23:38:12 -0800

    updates hashing playground file w3ith bcrypt example

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,36 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
+const bcrypt = require('bcryptjs');
+
+let password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt)=> {
+//     bcrypt.hash(password, salt, (err, hash)=> {
+//         console.log(hash);
+//     })
+// });
+
+
+const hashedPassword = '$2a$10$Q3gsS8hgQy/PQNYZn4Q4N.tKE8ju3duG3k4OfMYe87c3diRc4hLUW';
+
+bcrypt.compare(password, hashedPassword, (err, res)=> {
+    console.log(res);
+})
+
+
 //jwt.sign
 //jwt.verify
 
-const data = {
-    id: 10
-};
+// const data = {
+//     id: 10
+// };
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
 
-const decoded = jwt.verify(token, '123abcc');
-console.log('decoded', decoded);
+// const decoded = jwt.verify(token, '123abcc');
+// console.log('decoded', decoded);
 
 // const message ='I am user number 3';
 // const hash = SHA256(message).toString();
